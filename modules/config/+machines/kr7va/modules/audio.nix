b215999759698ab22ecfb5803140e6143b2f3cb4
declnix <20567435+declnix@users.noreply.github.com>
2026-07-05 18:00:00 +0200

    kr7va: add audio support

---
+++ modules/config/+machines/kr7va/modules/audio.nix
@@ -0,0 +1,17 @@
+{ ... }:
+{
+  den.aspects.kr7va.provides.declnix.nixos = {
+    security.rtkit.enable = true;
+
+    services.pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      jack.enable = true;
+      wireplumber.enable = true;
+    };
+
+    hardware.pulseaudio.enable = false;
+  };
+}
